e8fbbe90bd5f1a76f5edad39bbd2c3e7184d2bf8
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-02-17 21:11:53 +0100

    Create Register Features

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -21,9 +23,17 @@ class AuthController extends Controller
         # code...
     }
 
-    public function store(Request $request)
-    {
-        # code...
+    public function store(Request $request){
+        $validate = $request->validate([
+            'name' => 'required',
+            'email' => 'required|unique:users,email',
+            'password' => 'required',
+            'confirm-password' => 'required|same:password'
+        ]);
+        $data = $request->except('confirm-password', 'password');
+        $data['password'] = Hash::make($request->password);
+        User::create($data);
+        return redirect('/login');
     }
 
     public function lgout(Request $request)
